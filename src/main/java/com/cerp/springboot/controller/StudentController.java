3833d234f2ed79a690f8bc1f34f62affc846bc46
GouthamGuna <ggowthamsankar@gmail.com>
2022-09-04 10:30:32 +0530

    9_commit

--- src/main/java/com/cerp/springboot/controller/StudentController.java
+++ src/main/java/com/cerp/springboot/controller/StudentController.java
@@ -10,14 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cerp.springboot.model.Student;
 import com.cerp.springboot.service.StudentService;
 
 @RestController
-@RequestMapping("/api/student")
+//@RequestMapping
 public class StudentController {
 	
 	private StudentService studentservice;
@@ -28,34 +27,34 @@ public class StudentController {
 	}
 	
 	//build create student REST API
-	@PostMapping
+	@PostMapping("/")
 	public ResponseEntity<Student> savestudent(@RequestBody Student student){
 		System.out.println("Inside the Method...");
 		return new ResponseEntity<Student>(studentservice.saveStudent(student), HttpStatus.CREATED);
 	}
 
 	//build get all student REST API
-	@GetMapping
+	@GetMapping("/students")
 	public List<Student> getAllStudents(){
 		return studentservice.getAllStudent();
 	}
 	
 	//build get student by Id REST API
 	//http://localhost:8080/api/student/id 
-	@GetMapping("{id}") //path variable is dynamic
+	@GetMapping("/students/{id}") //path variable is dynamic
 	public ResponseEntity<Student> getStudedentId(@PathVariable("id") int studentId){
 		return new ResponseEntity<Student>(studentservice.getStudentById(studentId), HttpStatus.OK);
 	}
 	
 	//build update student REST API
-	@PutMapping("{id}")
+	@PutMapping("/students/{id}")
 	public ResponseEntity<Student> updateStudent(@PathVariable("id") int studentId
 			,@RequestBody Student student){
 		return new ResponseEntity<Student>(studentservice.updateStudent(student, studentId), HttpStatus.OK);
 	}
 	
 	//build delete student REST API
-	@DeleteMapping("{id}")
+	@DeleteMapping("/students/{id}")
 	public ResponseEntity<String> deleteStudent(@PathVariable("id") int studentId){
 		//delete student
 		studentservice.deleteStudent(studentId);

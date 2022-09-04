ee0c781cd42b74bf9a40f531ce1b02d3d84701ce
GouthamGuna <ggowthamsankar@gmail.com>
2022-09-04 10:39:41 +0530

    Only API

--- src/main/java/com/cerp/springboot/app/DemoCmsApplication.java
+++ src/main/java/com/cerp/springboot/app/DemoCmsApplication.java
@@ -2,10 +2,8 @@ package com.cerp.springboot.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("com.cerp.springboot.model")
 public class DemoCmsApplication {
 
 	public static void main(String[] args) {
